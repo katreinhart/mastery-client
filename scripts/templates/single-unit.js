9242a23a1ba92f0ca4da84c71828d06d9f5e0a77
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-14 14:42:38 -0800

    Improve consistenty of layout across course/unit/lesson

--- scripts/templates/single-unit.js
+++ scripts/templates/single-unit.js
@@ -13,14 +13,14 @@ const singleUnitTemplate = (unit, lessons) => {
         <div class="col-sm-6 mt-3 mb-2 text-center"><h3>${title}</h3></div>
       </div>
       <div class="row">
-        <div class="col-sm-6 col-lg-4">
+        <div class="col-sm-4 col-lg-3">
           <ul class="list-group">
             ${lessonList}
             <li class="list-group-item list-group-item-info"><a href="#/courses/${unit.course_id}/units/${id}/lessons/new">Add New Lesson</a></li>
           </ul>
           <a href="#/courses/${unit.course_id}" class="btn lime darken-2 btn-block">Back to Course</a>
         </div>
-        <div class="col-sm-6 col-lg-8">
+        <div class="col-sm-8 col-lg-9">
           <p>${summary}</p>
           <a href="#/courses/${unit.course_id}/units/${id}/edit" class="btn blue darken-4">Edit Unit</a>
         </div>
