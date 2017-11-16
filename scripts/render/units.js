78ac4988fdc13142b67dc10c451a3168f7f17cdd
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-16 09:40:24 -0800

    Debug db display issues on heroku

--- scripts/render/units.js
+++ scripts/render/units.js
@@ -36,14 +36,11 @@ const displayUnitForm = (courseId, unitId) => {
 
 const handleUnitFormSubmit = (e) => {
   e.preventDefault()
-  console.log('attempt')
   const title = document.getElementById('unitTitle').value
   const summary_text = document.getElementById('unitSummary').value
   const unitId = parseHash()[3]
-  console.log(title, summary_text)
   const courseId = parseHash()[1]
   if(unitId === 'new') {
-    console.log('making a new one')
     CourseUnit.create(courseId, { title, summary_text }).then(response => {
       const { unit } = response.data
       displayOneUnit(courseId, unit.id)
