549513d1b75f28fea0e85c924eb9392e49b54a2d
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-16 12:26:05 -0800

    Add favicon

--- scripts/templates/single-course.js
+++ scripts/templates/single-course.js
@@ -9,7 +9,7 @@ const singleCourseTemplate = (course, units) => {
     </div>
     <div class="row">
       <div class="col-sm-4 col-lg-3">
-        <ul  class="list-group">
+        <ul class="list-group">
           ${unitsList}
           <li class="list-group-item list-group-item-info"><a href="#/courses/${id}/units/new" class="indigo-text"><strong>Create new unit</strong></a></li>
         </ul>
